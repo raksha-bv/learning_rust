a145b26a9d72337879a9ebceecdc7aabf56f6a48
raksha-bv <rakshabv04@gmail.com>
2025-01-02 13:21:20 +0530

    functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,29 @@
 fn main() {
-    another_function(5);
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
+    print_labeled_measurement(5, 'h');
+
+    let x = five();
+
+    println!("The value of x is: {x}");
+
+    let z = plus_one(5);
+
+    println!("The value of z is: {z}");
 }
 
-fn another_function(x: i32) {
-    println!("The value of x is: {x}");
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
