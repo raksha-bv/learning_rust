ad083d7f7535cff92e8646866b2ee204c982f8a2
raksha-bv <rakshabv04@gmail.com>
2025-01-07 11:08:47 +0530

    hashmaps

---
+++ hashmaps/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+    println!("{scores:?}");
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+    println!("Score - {} for team {}", score, team_name);
+
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{map:?}");
+}
